71afbaeb1d693a50350d0d0c06242cf907192a96
Chonlakorn Punphopthaworn <chonlakorn.pun@gmail.com>
2019-04-26 20:03:47 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Grommet } from 'grommet';
 
 function App() {
+  const theme = {
+    global: {
+      font: {
+        family: 'Roboto',
+        size: '14px',
+        height: '20px',
+      },
+    },
+  };
+
   return (
-    <div className="App">
+    <Grommet theme={theme}>
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,7 +27,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </Grommet>
   );
 }
 
